88f67f2b71fc6a0428cb510e9f77dd7834157f4f
Hannes Dohrn <hannes.dohrn@fau.de>
2013-06-28 20:04:04 +0200

    AstNodeConverterBase now uses a cache to speed-up getter lookups

--- ptk-common/src/main/java/de/fau/cs/osr/ptk/common/serialization/AstNodeConverterBase.java
+++ ptk-common/src/main/java/de/fau/cs/osr/ptk/common/serialization/AstNodeConverterBase.java
@@ -20,8 +20,11 @@ package de.fau.cs.osr.ptk.common.serialization;
 import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import de.fau.cs.osr.ptk.common.ast.AstNode;
 import de.fau.cs.osr.ptk.common.ast.AstNodePropertyIterator;
@@ -33,6 +36,17 @@ public class AstNodeConverterBase<T extends AstNode<T>>
 		extends
 			AstConverterBase
 {
+	private static final int LOWER_CAPACITY = 256;
+	
+	private static final int UPPER_CAPACITY = 384;
+	
+	private static final float LOAD_FACTOR = .6f;
+	
+	private static final ConcurrentHashMap<Getter, Getter> CACHE =
+			new ConcurrentHashMap<Getter, Getter>(LOWER_CAPACITY, LOAD_FACTOR);
+	
+	// =========================================================================
+	
 	private final Class<T> nodeType;
 	
 	private NodeFactory<T> nodeFactory = new SimpleNodeFactory<T>();
@@ -264,14 +278,142 @@ public class AstNodeConverterBase<T extends AstNode<T>>
 	
 	protected Class<?> getGetterType(AstNode<T> n, String name)
 	{
+		Class<?> nodeType = n.getClass();
+		
+		Getter getter = new Getter(nodeType, name);
+		Getter cached = CACHE.get(getter);
+		if (cached != null)
+			return cached.getGetterType();
+		
+		return analyzeGetter(name, nodeType);
+	}
+	
+	private Class<?> analyzeGetter(String name, Class<?> nodeType)
+	{
+		Method getterMethod;
 		try
 		{
-			Method getter = new PropertyDescriptor(name, n.getClass()).getReadMethod();
-			return getter.getReturnType();
+			getterMethod = new PropertyDescriptor(name, nodeType).getReadMethod();
 		}
 		catch (IntrospectionException e)
 		{
-			throw new IncompatibleAstNodeClassException("Class '" + n.getClass().getName() + "' is malformed", e);
+			throw new IncompatibleAstNodeClassException("Class '" + nodeType.getName() + "' is malformed", e);
+		}
+		
+		Class<?> getterType = getterMethod.getReturnType();
+		rememberGetter(name, nodeType, getterType);
+		return getterType;
+	}
+	
+	private void rememberGetter(
+			String name,
+			Class<?> nodeType,
+			Class<?> getterType)
+	{
+		Getter getter = new Getter(nodeType, name, getterType);
+		Getter cached = CACHE.putIfAbsent(getter, getter);
+		if (cached == null)
+		{
+			// Make sure the target is not swept from the cache ...
+			getter.touch();
+			if (CACHE.size() > UPPER_CAPACITY)
+				sweepCache();
+		}
+	}
+	
+	private static synchronized void sweepCache()
+	{
+		if (CACHE.size() <= UPPER_CAPACITY)
+			return;
+		
+		Getter keys[] = new Getter[CACHE.size()];
+		
+		Enumeration<Getter> keysEnum = CACHE.keys();
+		
+		int i = 0;
+		while (i < keys.length && keysEnum.hasMoreElements())
+			keys[i++] = keysEnum.nextElement();
+		
+		int length = i;
+		Arrays.sort(keys, 0, length);
+		
+		int to = length - LOWER_CAPACITY;
+		for (int j = 0; j < to; ++j)
+			CACHE.remove(keys[j]);
+	}
+	
+	// =========================================================================
+	
+	protected static final class Getter
+			implements
+				Comparable<Getter>
+	{
+		private static long useCounter = 0;
+		
+		private long lastUse = -1;
+		
+		private Class<?> nodeType;
+		
+		private String fieldName;
+		
+		private Class<?> getterType;
+		
+		public Getter(Class<?> nodeType, String fieldName)
+		{
+			this.nodeType = nodeType;
+			this.fieldName = fieldName;
+			this.getterType = null;
+		}
+		
+		public Getter(Class<?> nodeType, String fieldName, Class<?> getterType)
+		{
+			this.nodeType = nodeType;
+			this.fieldName = fieldName;
+			this.getterType = getterType;
+		}
+		
+		public Class<?> getNodeType()
+		{
+			return nodeType;
+		}
+		
+		public String getFieldName()
+		{
+			return fieldName;
+		}
+		
+		public Class<?> getGetterType()
+		{
+			return getterType;
+		}
+		
+		@Override
+		public int hashCode()
+		{
+			return fieldName.hashCode() + 31 * nodeType.hashCode();
+		}
+		
+		@Override
+		public boolean equals(Object obj)
+		{
+			Getter other = (Getter) obj;
+			if (!fieldName.equals(other.fieldName))
+				return false;
+			if (nodeType != other.nodeType)
+				return false;
+			return true;
+		}
+		
+		public void touch()
+		{
+			lastUse = ++useCounter;
+		}
+		
+		@Override
+		public int compareTo(Getter o)
+		{
+			// Equality is not possible!
+			return (lastUse < o.lastUse) ? -1 : +1;
 		}
 	}
 }
